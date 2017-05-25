1162c0c639e71837c6d42df2ed0aa02b4692a2d0
Zach Toogood <Zach.Toogood@morningstar.com>
2017-05-25 18:45:46 +0100

    - Cleaned up imgui libs - Cleaned up common imports

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,26 +1,4 @@
-#include <stdio.h>
-#include <iostream>
-
-//https://github.com/nothings/stb/blob/master/stb_image.h
-// Defined in the build script
-//#define STB_IMAGE_IMPLEMENTATION 
-#include "stb_image.h"
-
-//https://github.com/agauniyal/rang
-#include "rang.hpp"
-
-//https://github.com/nlohmann/json/tree/master
-#include "json.hpp"
-using json = nlohmann::json;
-
-//https://github.com/Dav1dde/glad
-#include "glad/glad.h"
-
-//https://github.com/ocornut/imgui
-#include "imgui.h"
-#include "imgui_impl_sdl_gl3.h"
-
-#include "SDL.h"
+#include "common.h"
 
 int main(int argc, char* argv[]) 
 { 
